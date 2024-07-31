422ac88f188ed8db5ec2892f56daa9ae8c6f91b4
acquahsamuel <iamsamuelacquah@gmail.com>
2024-07-31 10:52:33 +0000

    WIP] +>  frontend build

--- src/app/modules/homepage/pages/home/home.component.ts
+++ src/app/modules/homepage/pages/home/home.component.ts
@@ -1,36 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { CategoryService } from 'src/app/shared/services/category.service';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { environment as env } from 'src/environments/environment';
 // import { JobService } from 'src/app/shared/services/job.service';
  
 
-const PRODUCTS = [
+const TESTIMONIALS = [
   {
     tag : "Phone",
+    name : "Phone",
     image : "https://www.apple.com/v/iphone/home/bu/images/overview/consider/personalize__f51wh6aiosya_xlarge.jpg",
     date : "1st June 2024",
-    title : "HP Gaming Laptop. Performance testinig for Speed and strength"
+    message : "I have consistently chosen KSarfo Computers for my laptop needs due to their impressive track record of quality and reliability. Every product I've purchased has been excellent, with no faults or issues. Their commitment to customer satisfaction is commendable, and I recommend them without reservation."
   },
   {
-    tag : "Laptop",
+    tag : "CEO of Trenda Africa",
+    name : "Samuel Acquah",
     image : "/assets/envy/photo_2024-06-10_15-49-52.jpg",
     date : "1st June 2024",
-    title : " HP Gaming Laptop. Performance testinig for Speed and strength"
+    message : "I have purchased several laptops from KSarfo Computers and have consistently been impressed with the quality of their products. Each purchase has been faultless, and I have never encountered any issues. Their commitment to providing genuine, high-quality products is evident. I wholeheartedly recommend their services to anyone in need of reliable computing solutions."
   },
   {
     tag : "Earpiece",
+    name : "Prince Obeng",
     image : "https://www.apple.com/v/iphone/home/bu/images/overview/consider/innovation__b03hhgj3xx9e_xlarge.jpg",
     date : "1st June 2024",
-    title : " HP Gaming Laptop. Performance testinig for Speed and strength"
+    message : " I've had the pleasure of purchasing multiple laptops from KSarfo Computers, and each time has been a seamless experience. The products are reliable and meet high standards of quality. Their professionalism and customer support make them a top choice for technology purchases."
   },
   {
     tag : "Earpiece",
+    name : "Alice Boakyewaa",
     image : "https://www.apple.com/v/iphone/home/bu/images/overview/consider/innovation__b03hhgj3xx9e_xlarge.jpg",
     date : "1st June 2024",
-    title : " HP Gaming Laptop. Performance testinig for Speed and strength"
+    message : "KSarfo Computers is a standout in the field of technology sales. Their laptops are of superb quality, and I have never faced any issues with the products I've bought. Their genuine approach and effective customer service make them highly recommendable"
   }
 ]
 
@@ -39,6 +44,7 @@ const PRODUCTS = [
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
   isLoading = false;
@@ -47,26 +53,14 @@ export class HomeComponent implements OnInit {
   jobIdx: any;
   ASSETURL = env.ASSETURL;
   SAMPLE_DATA = [];
-  PRODUCTS = PRODUCTS;
-  scenes = [1, 2, 3, 4];
-  currentScene = 1;
-  heroBackgroundImages = [
-    'url("https://ninetheme.com/themes/electron2/wp-content/uploads/2023/08/blog-posts-3-scaled-e1695291811949.jpg")',
-    'url("https://ninetheme.com/themes/electron2/wp-content/uploads/2023/08/blog-posts-2-scaled-e1695291769102.jpg")',
-    'url("https://ninetheme.com/themes/electron2/wp-content/uploads/2023/08/blog-post-1-scaled-e1695291695341-768x610.jpg")',
-    'url("https://ninetheme.com/themes/electron2/wp-content/uploads/2023/08/blog-posts-5-scaled-e1695291899694.jpg")'
-  ];
-
-  heroTexts = [
-    'HP',
-    'LENOVO',
-    'DELL',
-    'APPLE'
-  ];
-
+  TESTIMONIALS = TESTIMONIALS;
+  // PRODUCTS = PRODUCTS;
+  categories = [];
+  
   constructor(
     private authService: AuthService,
     public productService : ProductService,
+    private categoriesService : CategoryService,
     private router: Router
   ) {}
 
@@ -74,10 +68,9 @@ export class HomeComponent implements OnInit {
     this.isLoading = true;
     this.checkAuth();
     this.loadAllProducts();
+    this.loadAllCategories();
 
-
-    this.setCurrentSceneFromRoute();
-    // this.updateHeroBackground();
+ 
   }
  
 
@@ -108,29 +101,16 @@ export class HomeComponent implements OnInit {
   }
  
 
-
-  setCurrentSceneFromRoute() {
-    // Example: Extract scene from route params and set this.currentScene
-    // Replace with your actual logic to determine the current scene
-    // For example, if using ActivatedRoute:
-    // this.route.params.subscribe(params => {
-    //   this.currentScene = params['id']; // Adjust based on your route structure
-    // });
+  loadAllCategories() {
+    this.categoriesService.getAllCategories().subscribe(
+      (res: any) => {
+        console.log(res);
+        this.categories = res.categories;
+      }
+    )
   }
+
  
 
  
-
-  get heroBackgroundImage() {
-    return this.heroBackgroundImages[this.currentScene - 1]; // Adjust index
-  }
-
-  get heroText() {
-    return this.heroTexts[this.currentScene - 1]; // Adjust index
-  }
-
-  changeScene(scene: number) {
-    this.currentScene = scene;
-  }
-
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   enableDebug : false,
-  BASE_URL : 'https://backend-ksarfo-cw4y.onrender.com/api/v1',
-  // BASE_URL: 'http://localhost:4202/api/v1',
+  // BASE_URL : 'https://backend-ksarfo-cw4y.onrender.com/api/v1',
+  BASE_URL: 'http://localhost:4202/api/v1',
   UPLOAD_URL : 'https://general-upload-service.onrender.com',
   ASSETURL : "http://localhost:4202",
 };
